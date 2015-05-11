b767f979997c6d180d5f025afc07110a4473fb38
paulcottin <paulcottin@gmail.com>
2015-05-11 15:56:55 +0200

    commencement de IClass

--- src/rhapsodyClass/IClass.java
+++ src/rhapsodyClass/IClass.java
@@ -1,23 +1,48 @@
 package rhapsodyClass;
 
+import java.util.ArrayList;
+
 import parser.Noeud;
 import donnees.DiagrammeObjets;
 
 public class IClass extends DiagrammeObjets {
 
+	private ArrayList<String> attributs, operations;
+	
 	public IClass(Noeud n) {
 		super(n);
 	}
 	
 	@Override
 	public void initClass(){
+		attributs = new ArrayList<String>();
+		operations = new ArrayList<String>();
+		Noeud compNoeud = noeud.getChildByName("Compartments");
+		int sizeComp = compNoeud.getChildByName("size").getIntValue();
+		for (int i = 0; i < sizeComp; i++) {
+			if (getOutQuotes(compNoeud.getChildByName("value").getChilds().get(i).getChildByName("m_name").getStringValue()).equals("Attribute")){
+				Noeud attNoeud = compNoeud.getChildByName("value").getChilds().get(i).getChildByName("Items");
+				int sizeAtt = attNoeud.getChildByName("size").getIntValue();
+				for (int j = 0; j < sizeAtt; j++) {
+					System.out.println("ajout attribut "+i);
+				}
+			}
+			else if (getOutQuotes(compNoeud.getChildByName("value").getChilds().get(i).getChildByName("m_name").getStringValue()).equals("Operation")){
+				Noeud attNoeud = compNoeud.getChildByName("value").getChilds().get(i).getChildByName("Items");
+				int sizeAtt = attNoeud.getChildByName("size").getIntValue();
+				for (int j = 0; j < sizeAtt; j++) {
+					System.out.println("ajout operation "+i);
+				}
+			}
+		}
 		
+		//TODO Comment récupérer la couleur du block ?
 	}
 
 	@Override
 	protected void write() {
+		writeGeneral();
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
